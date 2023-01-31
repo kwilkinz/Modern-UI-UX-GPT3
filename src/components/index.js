a70f91216df34f065a0cde14c2dfe6073f97ad02
Keri Wilkins <keriannwilkins@gmail.com>
2023-01-31 17:51:33 -0600

    Navbar complete

---
+++ src/components/index.js
@@ -0,0 +1,10 @@
+/**
+ * Major exporting for:
+ * article, brand, cta, feature, navbar
+ */
+
+export { default as Article } from "./article/Article";
+export { default as Brand } from "./brand/Brand";
+export { default as CTA } from "./cta/CTA";
+export { default as Feature } from "./feature/Feature";
+export { default as Navbar } from "./navbar/Navbar";
---
+++ src/containers/index.js
@@ -0,0 +1,12 @@
+/**
+ * Containers
+ * Major exporting for:
+ * blog, feature, footer, header, possibility, whatGPT3
+ * this allows for faster exporting/retrieval times.
+ */
+export { default as Blog } from "./blog/Blog";
+export { default as Features } from "./features/Features";
+export { default as Footer } from "./footer/Footer";
+export { default as Header } from "./header/Header";
+export { default as Possibility } from "./possibility/Possibility";
+export { default as WhatGPT3 } from "./whatGPT3/WhatGPT3";
